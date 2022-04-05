d09a53aa0ba1b7473afc895b63a88f2a71153e38
Ridwan <ridwanediallo99@gmail.com>
2022-04-05 11:31:21 +0000

    Using the callbackhook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import './App.css';
 import github from './db';
-import { useEffect } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import query from './Query';
+
 
 function App() {
-  useEffect(() => {
-    const githubQuery = {
-      query: `
-      {
-        viewer {
-          name
-        }
-      }
-      `,
-    };
+  const [userName, setUserName] = useState('')
 
+  const fetchData = useCallback(() => {
     fetch(github.baseURL, {
       method: 'POST',
       headers: github.headers,
-      body: JSON.stringify(githubQuery),
+      body: JSON.stringify(query),
     })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err))
+      .then((response) => response.json())
+      .then((data) => {
+        setUserName(data.data.viewer.name);
+        console.log(data)
+      })
+      .catch((err) => console.log(err));
   }, []);
 
-  return <div className="App"></div>;
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return <div className="App">
+    <p>Hello from {userName}</p>
+  </div>;
 }
 
 export default App;
+
+
+
+
+
+
 
